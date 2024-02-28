4d6a2bf053838005fc709bb38446d2f0f2b06d1d
ycpeng-win <ycpeng@bu.edu>
2024-02-28 00:00:02 -0500

    section 18 project milestone

---
+++ section_18_project/src/components/Meals.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  async function fetchMeals() {
+    const response = await fetch("http://localhost:3000/meals");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch meals");
+    }
+
+    const meals = await response.json();
+    setLoadedMeals(meals);
+  }
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+  
+      if (!response.ok) {
+        throw new Error("Failed to fetch meals");
+      }
+  
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
+}
---
+++ section_18_project/src/http.js
@@ -0,0 +1,10 @@
+export async function fetchMeals() {
+  const response = await fetch("http://localhost:3000/meals");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch meals');
+  }
+
+  return resData;
+}
